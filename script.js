3078ce4b6ed8d63cde82fae628351ea7c74a27e9
Joao <joaokwai2conta@gmail.com>
2024-12-10 00:29:57 -0200

    adicionando logo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+AOS.init({
+    duration: 1000,
+    once: true,
+  });
 
+
+  const progressBars = document.querySelectorAll(".progress-bar");
+  window.addEventListener("scroll", () => {
+    progressBars.forEach((bar) => {
+      const barPosition = bar.getBoundingClientRect().top;
+      const screenPosition = window.innerHeight;
+      if (barPosition < screenPosition) {
+        bar.style.width = bar.getAttribute("data-value") + "%";
+      }
+    });
+  });
